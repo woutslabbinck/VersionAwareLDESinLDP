18b2ec1d9bd15c00ce4b0ccb0e76caf7bc3b0776
woutslabbinck <wout.slabbinck@ugent.be>
2022-11-17 13:52:12 +0100

    test: add integration test for status

--- test/integration/VLIL.test.ts
+++ test/integration/VLIL.test.ts
@@ -5,25 +5,59 @@ import {VLILConfig} from "../../src/metadata/VLILConfig";
 import {VersionAwareLDESinLDP} from "../../src/versionawarelil/VersionAwareLDESinLDP";
 import {MetadataParser} from "../../src/metadata/MetadataParser";
 import {DCT} from "../../src/util/Vocabularies";
+import {Status} from "../../src/ldes/Status";
 
 describe('An LDESinLDP', () => {
-    const lilIdentifier = baseUrl + 'vlil/'
+    const vlilIdentifier = baseUrl + 'vlil/'
 
     const communication = new LDPCommunication()
-    const ldesinldp = new LDESinLDP(lilIdentifier, communication)
+    const ldesinldp = new LDESinLDP(vlilIdentifier, communication)
     const vlil = new VersionAwareLDESinLDP(ldesinldp)
 
     const config: VLILConfig = {
         treePath: "http://schema.org/timestamppath",
         versionOfPath: "http://schema.org/versionofPath"
     }
+    describe('when checking the status of a vlil.', () => {
+        const lilIdentifier = baseUrl + "vlil_status_lil/"
+        const vlilIdentifier = baseUrl + "vlil_status/"
+        let lil: LDESinLDP
+        let vlil: VersionAwareLDESinLDP
+        let status: Status
 
+        beforeAll(async () => {
+            lil = new LDESinLDP(lilIdentifier, communication)
+            vlil = new VersionAwareLDESinLDP(new LDESinLDP(vlilIdentifier, communication))
+            await lil.initialise(config)
+            await vlil.initialise()
+        });
+        beforeEach(() => {
+            status = {empty: false, found: false, full: false, valid: false, writable: false}
+        });
+
+        it('returns found but not valid when it is a lil but not a vlil.', async () => {
+            const vlil = new VersionAwareLDESinLDP(lil)
+            const vlilStatus = await vlil.status()
+            status.found = true
+            status.empty = true
+            status.writable = true
+            expect(vlilStatus).toEqual(status)
+        });
+
+        it('returns found, valid, empty and writable when it is a new vlil just created.', async () => {
+            const vlilStatus = await vlil.status()
+            status.found = true
+            status.valid = true
+            status.empty = true
+            status.writable = true
+            expect(vlilStatus).toEqual(status)
+        });
+    });
     describe('when initialising a versioned LDES in LDP.', () => {
 
         it('succeeds with default config if no config is given.', async () => {
             const lilIdentifier = baseUrl + 'vlil_init_default/'
 
-            const communication = new LDPCommunication()
             const ldesinldp = new LDESinLDP(lilIdentifier, communication)
             const vlil = new VersionAwareLDESinLDP(ldesinldp)
 
